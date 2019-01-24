78e92c6aa42f3c66894aca467ab16695e8ea3e66
CrowdHailer <peterhsaxton@gmail.com>
2019-01-24 13:42:40 +0000

    implement as a middleware

--- lib/raxx/basic_authentication.ex
+++ lib/raxx/basic_authentication.ex
@@ -8,6 +8,24 @@ defmodule Raxx.BasicAuthentication do
 
   @authentication_header "authorization"
 
+  use Raxx.Middleware
+
+  @impl Raxx.Middleware
+  def process_head(request, config, next) do
+    case fetch_basic_authentication(request) do
+      {:ok, {user_id, password}} ->
+        if secure_compare(user_id, config.user_id) && secure_compare(password, config.password) do
+          {parts, next} = Raxx.Server.handle_head(next, request)
+          {parts, config, next}
+        else
+          {Raxx.separate_parts([unauthorized()]), config, next}
+        end
+
+      _ ->
+        {Raxx.separate_parts([unauthorized()]), config, next}
+    end
+  end
+
   @doc """
   Add a clients credentials to a request to authenticate it,
   using the 'Basic' HTTP authentication scheme.
@@ -75,12 +93,35 @@ defmodule Raxx.BasicAuthentication do
 
   - Validation should be added for the parameter values to ensure they only accept valid values.
   """
-  def unauthorized(options) do
+  def unauthorized(options \\ []) do
     realm = Keyword.get(options, :realm, @default_realm)
     charset = Keyword.get(options, :charset, @default_charset)
 
     Raxx.response(:unauthorized)
     |> Raxx.set_header("www-authenticate", BasicAuthentication.encode_challenge(realm, charset))
     |> Raxx.set_body("401 Unauthorized")
+  end
+
+  # This is only used because we have the dumb middleware version
+  @doc """
+  Compares the two binaries in constant-time to avoid timing attacks.
+  See: http://codahale.com/a-lesson-in-timing-attacks/
+  """
+  def secure_compare(left, right) do
+    if byte_size(left) == byte_size(right) do
+      secure_compare(left, right, 0) == 0
+    else
+      false
+    end
+  end
+
+  defp secure_compare(<<x, left::binary>>, <<y, right::binary>>, acc) do
+    import Bitwise
+    xorred = x ^^^ y
+    secure_compare(left, right, acc ||| xorred)
+  end
+
+  defp secure_compare(<<>>, <<>>, acc) do
+    acc
   end
 end
--- test/raxx/basic_authentication_test.exs
+++ test/raxx/basic_authentication_test.exs
@@ -3,4 +3,54 @@ defmodule Raxx.BasicAuthenticationTest do
   import Raxx
   import Raxx.BasicAuthentication
   doctest Raxx.BasicAuthentication
+
+  alias Raxx.Server
+
+  defmodule SimpleApp do
+    use Raxx.SimpleServer
+
+    @impl Raxx.SimpleServer
+    def handle_request(_request, _state) do
+      response(:ok)
+      |> set_body("Hello, World!")
+    end
+  end
+
+  setup do
+    stack =
+      Raxx.Stack.new(
+        [{Raxx.BasicAuthentication, %{user_id: "Aladdin", password: "open sesame"}}],
+        {SimpleApp, nil}
+      )
+
+    {:ok, stack: stack}
+  end
+
+  test "Request with valid authentication passes through stack", %{stack: stack} do
+    request =
+      request(:GET, "/")
+      |> set_header("authorization", "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==")
+
+    assert {[response], _} = Server.handle_head(stack, request)
+    assert response.status == 200
+    assert response.body == "Hello, World!"
+  end
+
+  test "Request with no authentication is denied", %{stack: stack} do
+    request = request(:GET, "/")
+
+    # TODO, what is the state of helpers here, can the middleware just return a response?
+    # assert {[response], _} = Server.handle_head(stack, request)
+    # assert response.status == 401
+  end
+
+  test "Request with invalid credentials is denied", %{stack: stack} do
+    request =
+      request(:GET, "/")
+      |> set_basic_authentication("Jafar", "Genie")
+
+    # TODO, what is the state of helpers here, can the middleware just return a response?
+    # assert {[response], _} = Server.handle_head(stack, request)
+    # assert response.status == 401
+  end
 end

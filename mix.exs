db1b3f41d5e0c97c7cc30b83f4538af9fa170f31
CrowdHailer <peterhsaxton@gmail.com>
2019-02-07 14:30:41 +0000

    prepare for hex

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule BasicAuthentication.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -21,6 +23,22 @@ defmodule BasicAuthentication.MixProject do
     [
       {:raxx, "~> 0.18.0"},
       {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp description do
+    """
+    Submit and verify client credentials using the 'Basic' HTTP authentication scheme.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Peter Saxton"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/crowdhailer/basic_authentication/tree/master/"
+      }
     ]
   end
 end

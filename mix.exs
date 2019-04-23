c82eaed8337da4eeec45e5d9287effa167d5ac91
CrowdHailer <peterhsaxton@gmail.com>
2019-04-23 12:56:34 +0100

    update dependencies

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BasicAuthentication.MixProject do
   def project do
     [
       app: :basic_authentication,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -21,7 +21,7 @@ defmodule BasicAuthentication.MixProject do
 
   defp deps do
     [
-      {:raxx, "~> 0.18.0"},
+      {:raxx, "~> 1.0"},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
